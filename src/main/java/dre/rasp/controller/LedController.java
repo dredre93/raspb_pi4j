f5006601b12015cfb94bc409ab6ec45bd89dcff3
dre <marco.drewes111@googlemail.com>
2018-03-20 19:26:12 +0100

    raspberry

---
+++ src/main/java/dre/rasp/controller/LedController.java
@@ -0,0 +1,28 @@
+package dre.rasp.controller;
+
+import com.pi4j.io.gpio.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class LedController {
+
+   private static GpioPinDigitalOutput pin;
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/light")
+    public String light(){
+
+        if (pin == null) {
+            GpioController gpio = GpioFactory.getInstance();
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "MyLed", PinState.LOW);
+
+        }
+
+        pin.toggle();
+
+        return "ok";
+    }
+}
