508747d35401fb88df811c766a7c21da1c8e5771
dredre93 <marco.drewes111@gmail.com>
2018-03-22 19:20:24 +0000

    import

--- src/main/java/dre/rasp/controller/LedController.java
+++ src/main/java/dre/rasp/controller/LedController.java
@@ -2,7 +2,7 @@ package dre.rasp.controller;
 
 import com.pi4j.io.gpio.*;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LedController {
